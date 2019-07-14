9d47ef86540dad88df618284b13ea2f004182997
Martin Nasierowski <martinnasierowski@Martins-MacBook-Pro.local>
2019-07-14 12:52:58 +0200

    reset

--- config/initializers/google_sign_in.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.configure do
-  config.google_sign_in.client_id     = "70175941273-4t72fp19i1l2uig3l87is6ujquedk8kd.apps.googleusercontent.com"
-  config.google_sign_in.client_secret = 'umjykoyC3iV-MHOL5i51OsuZ'
-end
