ffab4158db4348972ea1e61e26c413f12577fff8
Martin Nasierowski <martinnasierowski@Martins-MacBook-Pro.local>
2019-07-14 18:27:34 +0200

    add notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   def require_admin!
     unless current_user and current_user.role == 'admin'
-      redirect_to new_user_session_path, danger: "You don't belong there."
+      flash[:alert] = 'You dont belong there.'
+      redirect_to new_user_session_path
     end
   end
 end
